b35a8005352b60f3358f54124c679cd03df8eb0f
David A J Lee <daveajlee@users.noreply.github.com>
2011-01-08 11:53:31 +0000

    Minor bug fixes relating to vehicles.

--- trams/src/main/java/trams/util/SortedVehicles.java
+++ trams/src/main/java/trams/util/SortedVehicles.java
@@ -13,6 +13,7 @@ import trams.data.Vehicle;
 public class SortedVehicles implements Comparator<Vehicle> {
     
     public int compare (Vehicle o1, Vehicle o2 ) {
+    	System.out.println("Comparing registration number " + o1.getRegistrationNumber() + " with " + o2.getRegistrationNumber());
         return new String(o1.getRegistrationNumber()).compareTo(new String(o2.getRegistrationNumber()));
     }
 
